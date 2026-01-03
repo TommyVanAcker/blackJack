b2b8aabeab6881c0600c23ad6b3a6c680501a0bd
TommyVanAcker <tommyalphonsvanacker@gmail.com>
2026-01-03 10:55:58 +0100

    refactor first code

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 player_cards = []
 dealer_cards = []
 
-def deal_2_cards(deck):
-    for i in range(2):
-        card = random.choice(deck)
-        player_cards.append(card)
+def add_card(total, deck, cards):
+    for i in range(total):
+        card= random.choice(cards)
+        deck.append(card)
 
-def deal_computer_cards(deck):
-    card = random.choice(deck)
-    dealer_cards.append(card)
 
 def blackjack():
-    pass
+    print(f'your cards: {player_cards}, current score: {sum(player_cards)}')
+    print(f"Computer's first card: {dealer_cards[0]}")
+    deal = input("Type 'y' to get another card, type 'n' to pass:")
+    if deal == "y":
+        add_card(1, player_cards, cards)
 
 
 print(art.logo)
 game = input("Do you want to play a game of Blackjack? (y/n): ").lower()
 if game == "y":
-    deal_2_cards(cards)
-    deal_computer_cards(cards)
-
-print(f'your cards: {player_cards}, current score: {sum(player_cards)}')
-print(f"Computer's first card: {dealer_cards[0]}")
+    add_card(2, player_cards, cards)
+    add_card(1, dealer_cards, cards)
+    while True:
+        blackjack()
